34019bb2eac25b6b82034d5a3803f28b1b6d9680
alexeyTsk <alexey.tskhay@bk.ru>
2020-10-06 22:13:43 +0600

    2222

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("йльпщщфващьщзащовлаьщт");
     }
+
+    Scanner scanner = new Scanner(System.in);
+    int num = scanner.nextInt();
+
 }
